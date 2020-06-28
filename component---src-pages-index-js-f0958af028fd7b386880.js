1ed96dc709392c01d9759cb71b284c86c110b21d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-28 09:06:34 +0000

    Deploy devagr.me to github.com/devagrawal09/devagrawal09.github.io.git:master

---
+++ component---src-pages-index-js-f0958af028fd7b386880.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{RXBc:function(e,t,a){"use strict";a.r(t);var c=a("q1tI"),n=a.n(c),o=a("Bl7J");t.default=function(e){var t=e.location;return n.a.createElement(o.a,{location:t},n.a.createElement("section",{className:"page-section clearfix"},n.a.createElement("div",{className:"container"},n.a.createElement("h1",{className:"text-center wheat-text"},"Welcome to my site!"))))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-f0958af028fd7b386880.js.map
